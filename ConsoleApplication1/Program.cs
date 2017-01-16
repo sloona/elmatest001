f2c194e5b62f04b6ab60be85a8bc97d20151aac8
sloona <sloona@mail.ru>
2017-01-16 11:38:05 +0400

    первая версия

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calc;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calc.Calc();
+            int result = calc.Sum(1, 2);
+            Console.WriteLine($"{result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C = Calc;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// тестирование Calc
+    /// </summary>
+    [TestClass]
+    public class CalsUnitTest
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            var calc = new C.Calc();
+            var result = calc.Sum(1, 2);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
