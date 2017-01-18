eef429b5b0284acb906b11e9c5c040cd1ece3522
sloona <sloona@mail.ru>
2017-01-18 15:54:49 +0400

    Web application

---
+++ Web/Controllers/CalcController.cs
@@ -0,0 +1,29 @@
+﻿using Calc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            ViewData.Model = new OperationModel();
+            return View();
+        }
+
+        public ActionResult Execute(OperationModel model) {
+            var calc = new Calc.Calc(new IOperation[] { new SumOperation() });
+            //var result = calc.Execute(funcName, new object[] { x, y});
+            var result = calc.Execute(model.Name, model.GetParametrs());
+            ViewData.Model = $"result = {result}";
+            //return View("Execute", $"result = {result}");
+            return View();
+        } 
+    }
+}
---
+++ Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Web")]
+[assembly: AssemblyCopyright("© , 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("53cf79b8-108c-4ed8-a0ec-298ecb904f2b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
