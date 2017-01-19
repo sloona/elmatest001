2a45bcff947a80a40e30a26c291be88f1a0bf66a
sloona <sloona@mail.ru>
2017-01-19 16:41:10 +0400

    обновление перенос реализаций в отдельный класс

--- MyOperations/MyOperations/MyOperations/MyOperations.cs
+++ MyOperations/MyOperations/MyOperations/MyOperations.cs
@@ -47,4 +47,61 @@ namespace MyOperations
             return Convert.ToInt32(args[0]) * Convert.ToInt32(args[1]);
         }
     }
+
+    public class SumOperation : IOperation
+    {
+        public string Name { get { return "Sum"; } }
+        //public int ArgsNum { get { return 2; } }
+        public object Execute(object[] args)
+        {
+            var x = Convert.ToInt32(args[0]);
+            var y = Convert.ToInt32(args[1]);
+            //return (int)args[0] + (int)args[1];
+            return x + y;
+        }
+    }
+
+    /// <summary>
+    /// Умножение, два аргумента
+    /// </summary>
+    public class MultiplyOperation : IOperation
+    {
+        public string Name { get { return "Multiply"; } }
+        //public int ArgsNum { get { return 2; } }
+        public object Execute(object[] args)
+        {
+            try
+            {
+                return Convert.ToInt32(args[0]) * Convert.ToInt32(args[1]);
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка в операции " + Name + Environment.NewLine + ex;
+            }
+        }
+    }
+
+    /// <summary>
+    ///Возведение в квадрат, один аргумент 
+    /// </summary>
+    public class SquareOperation : IOperation
+    {
+        public string Name { get { return "Square"; } }
+        //public int ArgsNum { get { return 1; } }
+        public object Execute(object[] args)
+        {
+            try
+            {
+                return (int)args[0] * (int)args[0];
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка в операции " + Name + Environment.NewLine + ex;
+            }
+        }
+    }
+    /// <summary>
+    /// Сложение комплексных чисел, четыре аргумента
+    /// </summary>
+   
 }
